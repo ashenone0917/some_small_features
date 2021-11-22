803cdf339da0498196e0d09b3b75e6ea9a97a1cb
ashenonee <caiyanchao@datacloak.com>
2021-11-22 15:52:24 +0800

    first commit

---
+++ create_random_file_without_extension/mian.cpp
@@ -0,0 +1,92 @@
+#include <string>
+#include <random>
+#include <Windows.h>
+#include <iostream>
+using std::string;
+using std::wstring;
+using std::random_device;
+using std::default_random_engine;
+
+//不包含中文，只有大写
+string strRand(int length) {
+    char tmp;
+    string buffer;
+    buffer.reserve(length);
+    random_device rd;
+    default_random_engine random(rd());
+
+    for (int i = 0; i < length; i++) {
+        tmp = random() % 36;
+        if (tmp < 10) {
+            tmp += '0';
+        }
+        else {
+            tmp -= 10;
+            tmp += 'A';
+        }
+        buffer += tmp;
+    }
+    return buffer;
+}
+
+//包含中文大小写
+wstring wstrRand(int length) {			// length: 产生字符串的长度
+    wchar_t tmp;							// tmp: 暂存一个随机数
+    wstring buffer;						// buffer: 保存返回值
+    buffer.reserve(length);
+    // 下面这两行比较重要:
+    random_device rd;					// 产生一个 std::random_device 对象 rd
+    default_random_engine random(rd());	// 用 rd 初始化一个随机数发生器 random
+    auto chineseRange = 0x9FA5 - 0x4E00;
+    for (int i = 0; i < length; i++) {
+        auto isChinese = random() % 2;
+        if (isChinese) {
+            tmp = 0x4E00 + (random() % chineseRange);
+        }
+        else {
+            tmp = random() % 62;
+            if (tmp < 10) {
+                tmp += L'0';
+            }
+            else if (tmp < 36) {
+                tmp += L'A' - 10;
+            }
+            else {
+                tmp += L'a' - 36;
+            }
+        }
+        buffer += tmp;
+    }
+    return buffer;
+}
+
+
+int createFileRand(const std::wstring& file_path,int file_name_max,int file_count) {			// length: 产生字符串的长度
+    random_device rd;					// 产生一个 std::random_device 对象 rd
+    default_random_engine random(rd());	// 用 rd 初始化一个随机数发生器 random
+
+    int failCount = 0;
+    for (auto i = 0; i < file_count; ++i) {
+        auto fileLength = random() % file_name_max;
+        auto fileName = file_path + L"\\" + wstrRand(fileLength);
+        auto handle = CreateFile(fileName.c_str(), GENERIC_READ | GENERIC_WRITE,
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+            NULL,
+            CREATE_NEW,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL);
+        if (INVALID_HANDLE_VALUE == handle) {
+            auto lastErrorCode = ::GetLastError();
+            std::wcout << L"create file last error = "
+                << lastErrorCode << std::endl;
+            --i;
+            ++failCount;
+        }
+        else ::CloseHandle(handle);
+    }
+    return failCount;
+}
+int main() {
+    auto fail = createFileRand(L"D:\\filetemp", MAX_PATH,1000);
+    return 0;
+}
